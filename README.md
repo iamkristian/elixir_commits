c275e40e72b537be33784f2c71b1c355cac912a4
Kristian Rasmussen <kr@iamkristian.com>
2014-05-21 00:15:14 +0200

    Adding elixir commits

---
+++ README.md
@@ -0,0 +1,19 @@
+# ElixirCommits
+
+This module will fetch all commits from the github [elixir project](http://github.com/elixir-lang/elixir).
+
+# Usage
+
+```
+iex> ElixirCommits.GithubFetcher.fetch_page("1")
+```
+# The purpose
+
+This module serves as an example for a talk on Elixir Web development
+
+# Author
+
+Kristian Rasmussen
+
+License: MIT
+
---
+++ lib/elixir_commits/github_fetcher.ex
@@ -0,0 +1,26 @@
+defmodule ElixirCommits.GithubFetcher do
+  def fetch_page(page) do
+    body = get(page)
+    commits = extract_commits(body)
+    commits
+  end
+
+  defp extract_commits(body) do
+    # Extract all of the commit elements
+    lc e inlist body, do: e["commit"]
+  end
+
+  defp get(page) do
+    {:ok, 200, _headers, client} = :hackney.get(url_for(page))
+    {:ok, body} = :hackney.body(client)
+    ExJSON.parse(body, :to_map)
+  end
+
+  defp url_for(page) do
+    api_url <> page
+  end
+
+  defp api_url do
+    "https://api.github.com/repositories/1234714/commits?page="
+  end
+end
